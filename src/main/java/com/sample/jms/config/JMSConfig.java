67e61d0665cf4de12de2944c46c0785c5b05dd9c
aroraj08 <arorajatin08@gmail.com>
2020-08-06 16:20:24 -0700

    added object mapper

--- src/main/java/com/sample/jms/config/JMSConfig.java
+++ src/main/java/com/sample/jms/config/JMSConfig.java
@@ -1,5 +1,6 @@
 package com.sample.jms.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -13,12 +14,13 @@ public class JMSConfig {
     public static final String MY_SEND_RECV_QUEUE = "my-hello-world";
 
     @Bean
-    public MessageConverter messageConverter() {
+    public MessageConverter messageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter =
                 new MappingJackson2MessageConverter();
 
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(objectMapper);
         return converter;
     }
 }
